9072a9626466400e0319f6db6c041052e1facde7
Squad 5 <nago.thegloriouswalrus@gmail.com>
2023-03-28 18:48:39 -0500

    Reduce fuel consumption

--- config.lua
+++ config.lua
@@ -54,27 +54,27 @@ Config.RemoveHUDForBlacklistedVehicle = true
 
 -- Class multipliers. If you want SUVs to use less fuel, you can change it to anything under 1.0, and vise versa.
 Config.Classes = {
-	[0] = 1.0, -- Compacts
-	[1] = 1.0, -- Sedans
-	[2] = 1.0, -- SUVs
-	[3] = 1.0, -- Coupes
-	[4] = 1.0, -- Muscle
-	[5] = 1.0, -- Sports Classics
-	[6] = 1.0, -- Sports
-	[7] = 1.0, -- Super
-	[8] = 1.0, -- Motorcycles
-	[9] = 1.0, -- Off-road
+	[0] = 0.3, -- Compacts
+	[1] = 0.5, -- Sedans
+	[2] = 0.8, -- SUVs
+	[3] = 0.5, -- Coupes
+	[4] = 0.6, -- Muscle
+	[5] = 0.8, -- Sports Classics
+	[6] = 0.6, -- Sports
+	[7] = 0.5, -- Super
+	[8] = 0.5, -- Motorcycles
+	[9] = 0.5, -- Off-road
 	[10] = 1.0, -- Industrial
-	[11] = 1.0, -- Utility
-	[12] = 1.0, -- Vans
+	[11] = 0.8, -- Utility
+	[12] = 0.7, -- Vans
 	[13] = 0.0, -- Cycles
-	[14] = 1.0, -- Boats
-	[15] = 1.0, -- Helicopters
+	[14] = 0.5, -- Boats
+	[15] = 0.5, -- Helicopters
 	[16] = 1.0, -- Planes
 	[17] = 1.0, -- Service
-	[18] = 1.0, -- Emergency
+	[18] = 0.5, -- Emergency
 	[19] = 1.0, -- Military
-	[20] = 1.0, -- Commercial
+	[20] = 0.8, -- Commercial
 	[21] = 1.0, -- Trains
 }
 
